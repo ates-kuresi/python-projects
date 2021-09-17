57b62c64fee8dbf24569448c68cc7864b453e83a
ates-kuresi <ateskuresiz@gmail.com>
2021-09-17 17:22:06 +0300

    Update importfrom.py

--- importfrom.py
+++ importfrom.py
@@ -1,4 +1,6 @@
 import sys
-sys.path.insert(1, 'C:/Users/brkem/Desktop/keylogger')
-import idemail
-print(idemail.myMail)
+sys.path.insert(1, 'C:/Users/brkem/Desktop/keylogger') # Idk what that 1 does, when you put 0 it actually works too, give the destination of the file but not the actual file,
+import idemail # now we need to import the actual file which is idemail in my case, 
+print(idemail.myMail) # Trying if it works; it does
+
+'''In order to import files from another directory, you need to..'''
